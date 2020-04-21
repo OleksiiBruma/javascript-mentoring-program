7e86682ec32dd9e6fcc46fb09cb0ede7af159edc
Oleksii Bruma <oleksii_bruma@epam.com>
2020-04-21 19:12:42 +0300

    refactor reducers to seamless-immutable

--- courses-app/src/app/pages/courses/+state/courses.reducer.ts
+++ courses-app/src/app/pages/courses/+state/courses.reducer.ts
@@ -1,4 +1,5 @@
 import { createReducer, on } from "@ngrx/store";
+import { static as Immutable } from "seamless-immutable";
 import { initialCoursesState } from "./courses.state";
 import {
   loadCoursesSuccess,
@@ -17,59 +18,52 @@ import {
 } from "./courses.actions";
 
 export const coursesReducers = createReducer(
-  initialCoursesState,
-  on(loadCoursesSuccess, (state, { courses }) => ({
-    ...state,
-    courses: courses
-  })),
-  on(loadCoursesFailure, (state, { error }) => ({
-    ...state,
-    error: error
-  })),
-  on(deleteCourseSuccess, (state, { id }) => ({
-    ...state,
-    courses: state.courses.filter(course => course.id !== id)
-  })),
-  on(deleteCourseFailure, (state, { error }) => ({
-    ...state,
-    error: error
-  })),
-  on(updateCourseSuccess, (state, { course }) => ({
-    ...state,
-    courses: state.courses.filter(oldCourse =>
-      oldCourse.id === course.id ? course : oldCourse
+  Immutable.from(initialCoursesState),
+  on(loadCoursesSuccess, (state, { courses }) =>
+    Immutable.set(state, "courses", courses)
+  ),
+  on(loadCoursesFailure, (state, { error }) =>
+    Immutable.set(state, "error", error)
+  ),
+  on(deleteCourseSuccess, (state, { id }) =>
+    Immutable.set(
+      state,
+      "courses",
+      state.courses.filter(course => course.id !== id)
     )
-  })),
-  on(updateCourseFailure, (state, { error }) => ({
-    ...state,
-    error: error
-  })),
-  on(createCourseSuccess, (state, { course }) => ({
-    ...state,
-    courses: [...state.courses, course]
-  })),
-  on(createCourseFailure, (state, { error }) => ({
-    ...state,
-    error: error
-  })),
-  on(setFilterTerm, (state, { query }) => ({
-    ...state,
-    filterTerm: query
-  })),
-  on(setCurrentCourseSuccess, (state, { course }) => ({
-    ...state,
-    currentCourse: course
-  })),
-  on(setCurrentCourseFailure, (state, { error }) => ({
-    ...state,
-    error: error
-  })),
-  on(setPageType, (state, { pageType }) => ({
-    ...state,
-    pageType: pageType
-  })),
-  on(resetError, state => ({
-    ...state,
-    error: null
-  }))
+  ),
+  on(deleteCourseFailure, (state, { error }) =>
+    Immutable.set(state, "error", error)
+  ),
+  on(updateCourseSuccess, (state, { course }) =>
+    Immutable.set(
+      state,
+      "courses",
+      state.courses.filter(oldCourse =>
+        oldCourse.id === course.id ? course : oldCourse
+      )
+    )
+  ),
+  on(updateCourseFailure, (state, { error }) =>
+    Immutable.set(state, "error", error)
+  ),
+  on(createCourseSuccess, (state, { course }) =>
+    Immutable.set(state, "courses", Immutable(state.courses).concat([course]))
+  ),
+  on(createCourseFailure, (state, { error }) =>
+    Immutable.set(state, "error", error)
+  ),
+  on(setFilterTerm, (state, { query }) =>
+    Immutable.set(state, "filterTerm", query)
+  ),
+  on(setCurrentCourseSuccess, (state, { course }) =>
+    Immutable.set(state, "currentCourse", course)
+  ),
+  on(setCurrentCourseFailure, (state, { error }) =>
+    Immutable.set(state, "error", error)
+  ),
+  on(setPageType, (state, { pageType }) =>
+    Immutable.set(state, "pageType", pageType)
+  ),
+  on(resetError, state => Immutable.set(state, "error", null))
 );
